0374b9e5521fc717ba8b0f2847f09d8d446138da
Vitaliy <kompua@gmail.com>
2026-04-24 13:51:36 +0300

    Embed Google Tag Manager in document head

--- app/GoogleTag.tsx
+++ app/GoogleTag.tsx
@@ -1,6 +1,4 @@
-import Script from "next/script";
-
-function getGoogleTagManagerId() {
+export function getGoogleTagManagerId() {
   const explicitGtmId = (
     process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID ||
     process.env.NEXT_PUBLIC_GTM_ID ||
@@ -29,35 +27,13 @@ export default function GoogleTag() {
   }
 
   return (
-    <>
-      <Script
-        id="google-tag-manager"
-        strategy="beforeInteractive"
-      >
-        {`
-          window.dataLayer = window.dataLayer || [];
-          window.dataLayer.push({
-            'gtm.start': new Date().getTime(),
-            event: 'gtm.js'
-          });
-          (function(w,d,s,l,i){
-            var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),
-                dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${tagManagerId}');
-        `}
-      </Script>
-      <noscript>
-        <iframe
-          src={`https://www.googletagmanager.com/ns.html?id=${tagManagerId}`}
-          height="0"
-          width="0"
-          style={{ display: "none", visibility: "hidden" }}
-        />
-      </noscript>
-    </>
+    <noscript>
+      <iframe
+        src={`https://www.googletagmanager.com/ns.html?id=${tagManagerId}`}
+        height="0"
+        width="0"
+        style={{ display: "none", visibility: "hidden" }}
+      />
+    </noscript>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import GoogleTag from "./GoogleTag";
+import GoogleTag, { getGoogleTagManagerId } from "./GoogleTag";
 import GlobalLanguageSwitcher from "./GlobalLanguageSwitcher";
 import { buildMetadata, getRequestLanguage, seoCopy, siteUrl } from "../lib/seo";
 import "./globals.css";
@@ -33,9 +33,25 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   const language = await getRequestLanguage();
+  const tagManagerId = getGoogleTagManagerId();
 
   return (
     <html lang={language}>
+      <head>
+        {tagManagerId ? (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','${tagManagerId}');
+              `
+            }}
+          />
+        ) : null}
+      </head>
       <body>
         <GoogleTag />
         <GlobalLanguageSwitcher />
